38c61773af6d9c1c03721b973909a0db35829be8
Abenezer <abenezer89bayu@gmail.com>
2023-06-09 12:47:08 +0300

    Removed librosa dependency

--- predict.py
+++ predict.py
@@ -1,27 +1,29 @@
-import librosa
 import numpy as np
+from pydub import AudioSegment
+from python_speech_features import mfcc
 from tensorflow.keras.models import load_model
+from scipy.ndimage import zoom
 
 # Load model
 model = load_model('directions.h5')
 
 # Load new .wav file
 file_path = 'C:\\Users\\ABENEZER\\Documents\\repos\\Personal\\Directions-CNN\\dataSet\\Fit\\20.wav'
-sound, sample_rate = librosa.load(file_path)
+audio = AudioSegment.from_wav(file_path)
+audio_arr = np.array(audio.get_array_of_samples())
 
 # Extract MFCCs
-mfcc = librosa.feature.mfcc(y=sound, sr=sample_rate)
+mfcc_features = mfcc(audio_arr, samplerate=audio.frame_rate, nfft=1103).T
 
-# Pad MFCCs to match the input shape of the model
-max_len = model.input_shape[2]
-padded_mfcc = np.zeros((mfcc.shape[0], max_len))
-padded_mfcc[:, :mfcc.shape[1]] = mfcc
+# Resize the array to fixed size
+fixed_shape = (13, 100)  # This should be the same shape as you used while training
+mfcc_resized = zoom(mfcc_features, (fixed_shape[0] / mfcc_features.shape[0], fixed_shape[1] / mfcc_features.shape[1]))
 
 # Reshape to fit the network input (samples, bands, frames, channels)
-padded_mfcc = padded_mfcc.reshape(1, padded_mfcc.shape[0], padded_mfcc.shape[1], 1)
+mfcc_reshaped = mfcc_resized.reshape(1, mfcc_resized.shape[0], mfcc_resized.shape[1], 1)
 
 # Make prediction
-prediction = model.predict(padded_mfcc)
+prediction = model.predict(mfcc_reshaped)
 predicted_index = np.argmax(prediction[0])
 
 # Map index to corresponding label
--- trainDirection.py
+++ trainDirection.py
@@ -1,70 +1,72 @@
 import os
-import librosa
 import numpy as np
+from python_speech_features import mfcc
+from scipy.io import wavfile
+from scipy.ndimage import zoom
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
+from pydub import AudioSegment
+import random
 
-def augment_audio(audio, sample_rate):
-    # Pitch tuning
-    pitch_shift = np.random.uniform(low=-2.5, high=2.5)
-    audio_pitch_tuned = librosa.effects.pitch_shift(audio, sr=sample_rate, n_steps=pitch_shift)
 
-    # Time stretching
-    stretch_factor = np.random.uniform(low=0.8, high=1.2)
-    audio_time_stretched = librosa.effects.time_stretch(audio, rate=stretch_factor)
-
-    # Add noise
-    noise_factor = np.random.uniform(low=0.01, high=0.05)
-    white_noise = np.random.randn(len(audio))
-    audio_with_noise = audio + noise_factor * white_noise
+def augment_audio(audio):
+    # Noise addition
+    noise = AudioSegment.from_mono_audiosegments(
+        AudioSegment.silent(duration=len(audio)))
+    for _ in range(len(noise)):
+        noise = noise.overlay(audio, position=random.randint(0, len(audio)))
+    audio_with_noise = audio.overlay(noise)
 
     # Time shifting
-    shift = np.random.randint(sample_rate * 2)
-    shift_audio = np.roll(audio, shift)
+    shift_audio = audio[1000:] + audio[:1000]
 
-    return [audio_pitch_tuned, audio_time_stretched, audio_with_noise, shift_audio]
-
+    return [audio_with_noise, shift_audio]
 
 
 def preprocess_data(data_path, labels):
-    mfccs_list = []  # store mfccs
     y = []  # labels will go here
+
+    mfccs_list = []  # store mfccs
+
+    fixed_shape = (13, 100)
 
     for i, label in enumerate(labels):
         label_dir = os.path.join(data_path, label)
         for filename in os.listdir(label_dir):
             if filename.endswith('.wav'):
                 audio_path = os.path.join(label_dir, filename)
-                sound, sample_rate = librosa.load(audio_path)
-                augmented_sounds = augment_audio(sound, sample_rate)
-                for augmented_sound in augmented_sounds:
-                    mfcc = librosa.feature.mfcc(y=augmented_sound, sr=sample_rate)
-                    mfccs_list.append(mfcc)
-                    y.append(i)  # assign label index
+                audio = AudioSegment.from_wav(audio_path)
+                audio_arr = np.array(audio.get_array_of_samples())
+                mfcc_features = mfcc(audio_arr, samplerate=audio.frame_rate, nfft=1103).T
 
-    max_len = max(mfcc.shape[1] for mfcc in mfccs_list)
-    X = np.zeros((len(mfccs_list), mfccs_list[0].shape[0], max_len))
+                # Resize the array to fixed size
+                mfcc_resized = zoom(mfcc_features, (fixed_shape[0] / mfcc_features.shape[0], fixed_shape[1] / mfcc_features.shape[1]))
 
-    for i, mfcc in enumerate(mfccs_list):
-        X[i, :, :mfcc.shape[1]] = mfcc
+                mfccs_list.append(mfcc_resized)
+                y.append(i)  # assign label index
 
+    X = np.array(mfccs_list)
     y = np.array(y)
     y = to_categorical(y, num_classes=num_classes)
 
     return X, y
 
+
 def build_model(input_shape, num_classes):
     model = Sequential()
-    model.add(Conv2D(filters=64, kernel_size=2, input_shape=input_shape, activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=2,
+              input_shape=input_shape, activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(num_classes, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+    model.compile(loss='categorical_crossentropy',
+                  metrics=['accuracy'], optimizer='adam')
     return model
+
 
 # Define data path
 data_path = f'C:\\Users\\ABENEZER\\Documents\\repos\\Personal\\Directions-CNN\\dataSet\\'
@@ -77,15 +79,19 @@ num_classes = len(labels)
 X, y = preprocess_data(data_path, labels)
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 # Reshape to fit the network input (samples, bands, frames, channels)
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
+X_train = X_train.reshape(
+    X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
 
 # Build and train model
-model = build_model((X_train.shape[1], X_train.shape[2], X_train.shape[3]), num_classes)
-model.fit(X_train, y_train, batch_size=32, epochs=50, validation_data=(X_test, y_test), verbose=1)
+model = build_model(
+    (X_train.shape[1], X_train.shape[2], X_train.shape[3]), num_classes)
+model.fit(X_train, y_train, batch_size=32, epochs=50,
+          validation_data=(X_test, y_test), verbose=1)
 
 score = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: ", score[1])
@@ -94,5 +100,3 @@ print("Accuracy: ", score[1])
 model.save('directions.h5')
 
 print("Model trained and saved as directions.h5")
-
-
